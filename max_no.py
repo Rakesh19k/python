abd458db917d87b518a43029a70b22e816ae330f
Rakesh19k <rakesh19@navgurukul.org>
2020-02-07 09:45:37 +0530

    input binary

--- max_no.py
+++ max_no.py
@@ -1,8 +1,8 @@
-def max(number):
-	max_no=number[0]
-	for i in number:
-		if i>max_no:
-			max_no=i
-	return max_no
-number=input("enter the no:").split()
-print (max(number))
+# def max(number):
+# 	max_no=number[0]
+# 	for i in number:
+# 		if i>max_no:
+# 			max_no=i
+# 	return max_no
+# number=input("enter the no:").split()
+# print (max(number))
